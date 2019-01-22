1334a06030897cbf7d7ab8f9aa5f890d9990dbcb
Raphaël H <github@hraph.fr>
2019-01-22 14:37:30 +0000

    Reduce token length to 8 chars

--- src/models/ClientIdentifier.js
+++ src/models/ClientIdentifier.js
@@ -1,18 +1,17 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ClientType;
 (function (ClientType) {
     ClientType[ClientType["Worker"] = 0] = "Worker";
     ClientType[ClientType["RemoteCLI"] = 1] = "RemoteCLI";
-})(ClientType = exports.ClientType || (exports.ClientType = {}));
-var TaskStatus;
+})(exports.ClientType || (exports.ClientType = {}));
+var ClientType = exports.ClientType;
 (function (TaskStatus) {
     TaskStatus[TaskStatus["Idle"] = 0] = "Idle";
     TaskStatus[TaskStatus["Running"] = 1] = "Running";
     TaskStatus[TaskStatus["Error"] = 2] = "Error";
-})(TaskStatus = exports.TaskStatus || (exports.TaskStatus = {}));
-class ClientIdentifier {
-    constructor(groupId, instanceId) {
+})(exports.TaskStatus || (exports.TaskStatus = {}));
+var TaskStatus = exports.TaskStatus;
+var ClientIdentifier = (function () {
+    function ClientIdentifier(groupId, instanceId) {
         this.clientType = ClientType.Worker;
         this.clientId = null;
         this.latestReceivedPingTimestamp = 0;
@@ -23,15 +22,28 @@ class ClientIdentifier {
         this.instanceId = instanceId;
         this.token = this._generateHash();
     }
-    _generateHash() {
+    /**
+     * Generate unique key of 8 char
+     * @returns {string}
+     * @private
+     */
+    ClientIdentifier.prototype._generateHash = function () {
         return Math.random().toString(36).substring(2, 10);
-    }
-    getWorkerIdentity() {
+    };
+    /**
+     * Get identity for workers ONLY
+     * @returns {WorkerIdentity}
+     */
+    ClientIdentifier.prototype.getWorkerIdentity = function () {
         return this.identity;
-    }
-    setWorkerIdentity(identity) {
+    };
+    /**
+     * Set the identity wor workers ONLY
+     * @param {WorkerIdentity} identity
+     */
+    ClientIdentifier.prototype.setWorkerIdentity = function (identity) {
         this.identity = identity;
-    }
-}
+    };
+    return ClientIdentifier;
+}());
 exports.ClientIdentifier = ClientIdentifier;
-//# sourceMappingURL=ClientIdentifier.js.map
