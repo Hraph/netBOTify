32d4043987fb5684aac2b8e50bc3671ccd294141
Raphaël H <github@hraph.fr>
2020-01-19 15:45:57 +0100

    Update dependencies

--- dist/src/utils.d.ts
+++ dist/src/utils.d.ts
@@ -1,2 +1,2 @@
-export declare function promiseTimeout(ms: number, promise: Promise<any>): Promise<unknown>;
-export declare function promiseAllTimeout(ms: number, promiseArray: Promise<any>[]): Promise<unknown>;
+export declare function promiseTimeout(ms: number, promise: Promise<any>): Promise<{}>;
+export declare function promiseAllTimeout(ms: number, promiseArray: Promise<any>[]): Promise<{}>;
