92ce6c6d627395dfdabd0966c4f3309a41e581cc
Raphaël H <github@hraph.fr>
2020-01-28 19:09:18 +0100

    Update dist

--- dist/src/utils/utils.d.ts
+++ dist/src/utils/utils.d.ts
@@ -1,5 +1,5 @@
-export declare function promiseTimeout(ms: number, promise: Promise<any>): Promise<{}>;
-export declare function promiseAllTimeout(ms: number, promiseArray: Promise<any>[]): Promise<{}>;
+export declare function promiseTimeout(ms: number, promise: Promise<any>): Promise<unknown>;
+export declare function promiseAllTimeout(ms: number, promiseArray: Promise<any>[]): Promise<unknown>;
 export declare function reduceObjectToAllowedKeys(object: any, keys: any): Object;
 export declare function objectGroupByPropertyAndCount(objectArray: any[], prop: string): any;
 export declare function objectGroupByProperty(obj: any, prop: string): any;
